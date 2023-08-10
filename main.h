faa1bfcd7d4132343d185f76d209aa71025dcef8
Kiwizacxs <brian.deleon2016@outlook.com>
2023-08-10 13:56:06 +0000

    Simple Shell is comming

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef SHELL
 #define SHELL
+
+/*necessaries libraries*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+/*prototypes functions*/
+
 #endif
--- simple_shell.c
+++ simple_shell.c
@@ -1,51 +1,33 @@
-#include "usualies.h"
+#include "main.h"
 
 /**
-*main - function for a simple command-line interface.
-*
-* Simple command-line program that allows users to input and process commands.
+* main - function for a simple command-line interface.
 *
 * @return Returns 0 to indicate successful termination.
 */
 
-#define MAX_COMMAND_LENGTH 100  /* Defino la longitud máxima de un comando */
 
 int main(void)
 {
 	char *prompt = "#cisfun$ "; /*Cadena que representa el indicador del comando*/
-
-	/* Almacena el comando ingresado por el usuario */
-	char command[MAX_COMMAND_LENGTH];
+	size_t n = 0;
+	int characters;
+	char *lineptr;
 
 	while (1)  /* Bucle infinito */
 	{
 		printf("%s", prompt);  /* Imprime el indicador del comando */
-
-			/* Lee el comando desde la entrada estándar */
-		if (fgets(command, sizeof(command), stdin) == NULL)
-		{	/* Si se alcanzó el final del archivo de entrada (Ctrl+D) */
-			if (feof(stdin))
-			{
-				printf("\n");
-				break; /* Condición de fin de archivo (Ctrl+D) */
-			}
-			else
-			{
-				/* Imprime un mensaje de error si la lectura falla */
-				perror("Error de entrada");
-				exit(EXIT_FAILURE);  /* Termina el programa con un código de error */
-			}
-		}
-
-		/* Eliminar el salto de línea al final de la cadena ingresada */
-		/* Busca y reemplaza el carácter de nueva línea */
-		command[strcspn(command, "\n")] = '\0';
-
-		if (strcmp(command, "exit") == 0)  /* Compara el comando con "exit" */
+		characters = getline(&lineptr, &n, stdin);
+		if (characters == -1)
 		{
-			break;  /* Sale del bucle si el comando es "exit" */
+			break;
 		}
-	}
+		if (strcmp(lineptr, "exit\n") == 0)
+		{
+			break;
+		}
 
+	}
+	free(lineptr); /*libera lineptr*/
 	return (0);  /* Termina el programa con éxito */
 }
